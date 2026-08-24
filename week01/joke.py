13f58d0c30f5ffbff58a18a930efb2b0d340dfa0
John George <johngbauerbuis@gmail.com>
2026-08-24 13:10:56 -0500

    Add files via upload
    Just populating existing files.

---
+++ week01/joke.py
@@ -0,0 +1,10 @@
+# Assignment 1B: Input & Output
+"""
+Multi-line comment that is ignored by the compiler.
+"""
+
+print("Knock knock!")
+print("Who is there?")
+print("Interrupting cow.")
+print("Interrupting cow wh-MOO!")
+# Adapted from a joke I remember, I believe from "Boy's Life" magazine.
---
+++ week01/madlibs.py
@@ -0,0 +1,29 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included (Copy and paste THIS comment from opening to closing quotes).
+[ ] 2. Program asks for at least 5 different inputs (variables).
+[ ] 3. Output uses F-Strings to combine text and variables.
+[ ] 4. Output uses at least one escape sequence (\n or \t).
+[ ] 5. Code contains comments explaining the steps.
+[ ] 6. Program runs without errors.
+-----------------------------------------------------------------------
+"""
+
+name1 = input("Please enter a first name: ")
+name2 = input("Please enter another first name: ")
+name3 = input("Please enter a third first name: ")
+verb1 = input("Please enter an action verb: ")
+dessert1 = input("Please enter the name of a dessert: ")
+
+print(f"\nLadies and gentlemen, this is Mambo No. 5\n")
+print(f"One, two, three, four, five")
+print(f"Everybody in the car so come on let's {verb1}")
+print(f"To the candy-store around the corner")
+print(f"The boys say they want some {dessert1}")
+print(f"But I really don't wanna")
+print(f"We'll really party hearty to the mambo sound")
+print(f"I like {name1}, {name2}, {name3}, and Daisy!")
+print(f"And as I continue, the gang is goin' crazy...")
+print(f"\nThe End, for now.")
