c680e4ace8b3be06ed435b80cb3e9c88eea8f648
John George Bauer-Buis <jbauer-buis@students.mchenry.edu>
2026-08-31 10:54:38 -0500

    Add datatypes.py with assignment requirements and testing section

---
+++ week02/datatypes.py
@@ -0,0 +1,15 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS (DO NOT DELETE)
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Define a String variable.
+[ ] 3. Define an Integer variable.
+[ ] 4. Define a Float variable.
+[ ] 5. Define a Boolean variable.
+[ ] 6. Print all variables using F-Strings.
+[ ] 7. Upload to GitHub.
+-----------------------------------------------------------------------
+"""
+
+## Testing
