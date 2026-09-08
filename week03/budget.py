7678fccca7ecaedf548f8135678029cd7da23b7e
John George Bauer-Buis <jbauer-buis@students.mchenry.edu>
2026-09-07 21:33:56 -0500

    Created budget.py file.  WIP.

---
+++ week03/budget.py
@@ -0,0 +1,14 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Ask user for Monthly Income (float).
+[ ] 3. Ask user for 5 DIFFERENT expense amounts (float)(Rent, Utilities, etc.)
+[ ] 4. Calculate Total Expenses and Remaining Balance.
+[ ] 5. Calculate Percentage of Income Spent.
+[ ] 6. Output formatted to 2 decimal places (:,.2f or :.2%).
+-----------------------------------------------------------------------
+"""
+
+gross_income = float(input("Enter Monthly Gross Income: $"))
