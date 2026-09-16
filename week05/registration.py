2ade5259fb4333d00ac1932caba2d7b6f8618286
John George Bauer-Buis <jbauer-buis@students.mchenry.edu>
2026-09-16 12:23:52 -0500

    Added input validation for first and last names in registration.py plus additional comments describing what the program is supposed to do.

--- week05/registration.py
+++ week05/registration.py
@@ -5,11 +5,22 @@ ASSIGNMENT 5A: INPUT VALIDATION
 [ ] 1. Header Docstring included.
 [ ] 2. All 5 inputs have 'while' loop validation.
 [ ] 3. The more tickets loop uses .upper() and correct Boolean logic.
-[ ] 4. Include a try and except statement around the entire program. Should have one defined 
+[ ] 4. Include a try and except statement around the entire program. Should have one defined
        exception (probably value error) and a generic exception
 [ ] 5. Have pinned a variable in the WATCH window and took a screenshot.
 -----------------------------------------------------------------------
 """
-try
-	while()
-except
+
+## First Name & Last Name: Cannot be blank.
+first_name = input("Enter First Name: ")
+while first_name == "":
+    print("Error: Name cannot be blank.")
+    first_name = input("Please enter First Name: ")
+last_name = input("Enter Last Name: ")
+while last_name == "":
+    print("Error: Name cannot be blank.")
+    last_name = input("Please enter Last Name: ")
+
+## Age: Must be a number; also check whether they are older or younger than 21 to determine whether they get a drink ticket.
+## Phone Number: Cannot be blank.
+## Ticket Count: Must be a valid integer > 0 (Crash-Proof!).
