da4dc7d0909a29f62fa35460b9f6274fc54f0ba0
John George Bauer-Buis <jbauer-buis@students.mchenry.edu>
2026-09-23 11:33:01 -0500

    Created tickets.py file and started writing code.

---
+++ week06/tickets.py
@@ -0,0 +1,19 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT 6A: TICKET SALES
+-----------------------------------------------------------------------
+[ ] 1. Create a list of 20 seats (numbered 1-20).
+[ ] 2. Display the list of available seats.
+[ ] 3. Ask user for a seat number (0 to quit).
+[ ] 4. Remove the selected seat from the list.
+[ ] 5. Handle invalid inputs (seat taken or doesn't exist).
+[ ] 6. Repeat until user quits or seats are empty.
+-----------------------------------------------------------------------
+"""
+
+try:
+        choice = int(input("Please enter a seat number to reserve:"))
+    except ValueError:
+        print("Error! Please enter a valid number.")
+        continue
+    
