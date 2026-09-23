eb6296e37a0acad0dabf681a2132dd1b7e284c13
John George Bauer-Buis <jbauer-buis@students.mchenry.edu>
2026-09-23 13:34:46 -0500

    Refactor locked.py to improve menu display and user input handling.

--- week06/locked.py
+++ week06/locked.py
@@ -13,13 +13,21 @@ ASSIGNMENT 6B: THE DEPARTMENT SECURITY TERMINAL
 ## Declare variables that are known to be needed.
 ## The department constant, a tuple for the usernames.
 DESIGN
+USER_NAMES = ("Andy", "Bob", "Cathy", "Dennis")
 
 ## While loop surrounding the program.
 is_running = True
 while is_running:
-        ## Display the menu.
-
-
+    ## Display the menu.
+    name = input("Please enter your username:")
+    if name in USER_NAMES:
+        location = USER_NAMES.index(name)
+        password = input("Enter your new password:")
+        passwords[location] = password
+        print("Your password has been changed.")
+    else:
+        print("I'm sorry, that user does not exist.")
+    is_running = False  # This is here for testing purposes, so the program doesn't run forever.  Will be moved.
 
 """Error handling to catch if the user tries to add a username
 (a TypeError, since tuples cannot be modified in place)
