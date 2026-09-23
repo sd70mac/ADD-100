d05f270b24f83d4b5b2ab51c00d83740ca98cfe7
John George Bauer-Buis <jbauer-buis@students.mchenry.edu>
2026-09-23 11:30:27 -0500

    Created and started writing the locked.py file.

---
+++ week06/locked.py
@@ -0,0 +1,28 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT 6B: THE DEPARTMENT SECURITY TERMINAL
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included. Yes.
+[ ] 2. Department constant defined in ALL_CAPS.
+[ ] 3. Username tuple and password list defined.
+[ ] 4. While loop runs interactively.
+[ ] 5. Try/except catches TypeError and tells user to email help desk.
+-----------------------------------------------------------------------
+"""
+
+## Declare variables that are known to be needed.
+## The department constant, a tuple for the usernames.
+DESIGN
+
+## While loop surrounding the program.
+is_running = True
+while is_running:
+        ## Display the menu.
+
+
+
+"""Error handling to catch if the user tries to add a username
+(a TypeError, since tuples cannot be modified in place)
+instructing the user to send an email to the help desk.
+More error handling to catch both a ValueError and an IndexError.
+"""
